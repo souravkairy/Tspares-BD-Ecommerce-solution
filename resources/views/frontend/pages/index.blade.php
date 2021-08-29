1c815484e6387f61a1ce31b272bda472cb0845dc
naeemchy <61413398+naeemchy@users.noreply.github.com>
2021-08-30 01:19:04 +0600

    form updated

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -49,7 +49,7 @@ $cat = DB::table('categories')->limit(8)->get();
                 </div>
             </div>
             <!-- Free Shoping items -->
-            <div class="row">
+            <div class="row justify-content-center">
                 @forelse ($cat as $item)
                     <div class="col-6 col-lg-3 col-md-3">
                         <div class="free_items">
@@ -288,7 +288,7 @@ $cat = DB::table('categories')->limit(8)->get();
 ========================================================-->
 <section id="flash_cell">
     <div class="flash_item">
-        <div class="container px-lg-0">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-4 col-md-4 col-xl-4 d-none d-md-block flash_head">
                     <h4>Flash Sale</h4>
@@ -336,20 +336,20 @@ $cat = DB::table('categories')->limit(8)->get();
                 <div class="col-md-2 col-sm-2">
                     <div class="flash_inner">
                         <div class="flash_img">
-                            <img src="{{ $plashproduct->p_f_img }}" alt="">
+                            <a href="{{ route('product.view', $plashproduct->id) }}"><img src="{{ $plashproduct->p_f_img }}" alt=""></a>
                             <div class="offer_tag">
                                 <p>49% OFF</p>
                             </div>
                         </div>
                         <a href="{{ route('product.view', $plashproduct->id) }}">
                             <h2>{{ $plashproduct->p_name }}</h2>
-                            @if ($plashproduct->p_o_price == null)
-                                <span>${{ $plashproduct->p_price }}</span>
-                            @else
-                                <span
-                                    style="margin-right: 5px;">${{ $plashproduct->p_price }}</span>$<del>{{ $plashproduct->p_o_price }}</del>
-                            @endif
                         </a>
+                        @if ($plashproduct->p_o_price == null)
+                            <span>${{ $plashproduct->p_price }}</span>
+                        @else
+                            <span
+                                style="margin-right: 5px;">${{ $plashproduct->p_price }}</span>$<del>{{ $plashproduct->p_o_price }}</del>
+                        @endif
                     </div>
                 </div>
                 @empty
