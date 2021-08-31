cc254a6f5dbfd351621f1aaf731a53d23b42a539
souravkairy <48748661+souravkairy@users.noreply.github.com>
2021-08-31 14:03:43 +0600

    section updated

--- resources/views/backend/admin/SiteSetting/sectionSetting.blade.php
+++ resources/views/backend/admin/SiteSetting/sectionSetting.blade.php
@@ -40,12 +40,12 @@
                                          @csrf
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="text" id="text" class="form-control" name="sec1Text" placeholder="Heading" value="{{$sectionSetting->sec1Text}}">
+                                                 <input type="text" id="text" class="form-control" name="sec1Text" placeholder="Heading" value="{{$sectionSetting->sec1Text}}" required>
                                              </div>
                                          </div>
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <select name="sec1catName" id="" class="form-control" value="{{$sectionSetting->sec1catName}}">
+                                                 <select name="sec1catName" id="" class="form-control" value="{{$sectionSetting->sec1catName}}" required>
                                                      <option value="">Select Category</option>
                                                      @forelse ($category as $item)
                                                          <option value="{{ $item->name }}">{{ $item->name }}
@@ -58,12 +58,12 @@
                                          </div>
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="file" id="file" class="form-control" name="sec1Image">
+                                                 <input type="file" id="file" class="form-control" name="sec1Image" required>
                                              </div>
                                          </div>
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="text" id="file" class="form-control" name="sec1BtnLink" placeholder="button Link" value="{{$sectionSetting->sec1BtnLink}}">
+                                                 <input type="text" id="file" class="form-control" name="sec1BtnLink" placeholder="button Link" value="{{$sectionSetting->sec1BtnLink}}" required>
                                              </div>
                                          </div>
                                          <div class="text-center">
@@ -89,22 +89,22 @@
                                          @csrf
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="text" id="text" class="form-control" name="sec2Heading" placeholder="Heading" value="{{$sectionSetting->sec2Heading}}">
+                                                 <input type="text" id="text" required class="form-control" name="sec2Heading" placeholder="Heading" value="{{$sectionSetting->sec2Heading}}">
                                              </div>
                                          </div>
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="text" id="text" class="form-control" name="sec2Text" placeholder="test" value="{{$sectionSetting->sec2Text}}">
+                                                 <input type="text" id="text" required class="form-control" name="sec2Text" placeholder="test" value="{{$sectionSetting->sec2Text}}">
                                              </div>
                                          </div>
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="file" id="file" class="form-control" name="sec2Image" value="{{$sectionSetting->sec2Image}}">
+                                                 <input type="file" id="file" required class="form-control" name="sec2Image" value="{{$sectionSetting->sec2Image}}">
                                              </div>
                                          </div>
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="text" id="file" class="form-control" name="sec2BtnLink" placeholder="button link" value="{{$sectionSetting->sec2BtnLink}}">
+                                                 <input type="text" id="file" required class="form-control" name="sec2BtnLink" placeholder="button link" value="{{$sectionSetting->sec2BtnLink}}">
                                              </div>
                                          </div>
                                          <div class="text-center">
@@ -130,17 +130,17 @@
                                          @csrf
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="text" id="text" class="form-control" name="sec3Text" placeholder="Heading" value="{{$sectionSetting->sec3Text}}">
+                                                 <input type="text" id="text" required class="form-control" name="sec3Text" placeholder="Heading" value="{{$sectionSetting->sec3Text}}">
                                              </div>
                                          </div>
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="file" id="file" class="form-control" name="sec3Image" value="{{$sectionSetting->sec3Image}}">
+                                                 <input type="file" id="file" required class="form-control" name="sec3Image" value="{{$sectionSetting->sec3Image}}">
                                              </div>
                                          </div>
                                          <div class="form-group row">
                                              <div class="col-sm-12">
-                                                 <input type="text" id="file" class="form-control" name="sec3BtnLink" placeholder="Button Link" value="{{$sectionSetting->sec3BtnLink}}">
+                                                 <input type="text" id="file" required class="form-control" name="sec3BtnLink" placeholder="Button Link" value="{{$sectionSetting->sec3BtnLink}}">
                                              </div>
                                          </div>
                                          <div class="text-center">
@@ -169,22 +169,22 @@
                                         @csrf
                                         <div class="form-group row">
                                             <div class="col-sm-12">
-                                                <input type="text" id="text" class="form-control" name="sec4Heading" placeholder="heading" value="{{$sectionSetting->sec4Heading}}">
+                                                <input type="text" id="text" class="form-control" required name="sec4Heading" placeholder="heading" value="{{$sectionSetting->sec4Heading}}">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <div class="col-sm-12">
-                                                <input type="text" id="text" class="form-control" name="sec4Text" placeholder="sub-heading" value="{{$sectionSetting->sec4Text}}">
+                                                <input type="text" id="text" class="form-control" required name="sec4Text" placeholder="sub-heading" value="{{$sectionSetting->sec4Text}}">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <div class="col-sm-12">
-                                                <input type="file" id="file" class="form-control" name="sec4Image" value="{{$sectionSetting->sec4Image}}">
+                                                <input type="file" id="file" class="form-control" required name="sec4Image" value="{{$sectionSetting->sec4Image}}">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <div class="col-sm-12">
-                                                <input type="text" id="file" class="form-control" name="sec2BtnLink" placeholder="button link" value="{{$sectionSetting->sec2BtnLink}}">
+                                                <input type="text" id="file" class="form-control" required name="sec2BtnLink" placeholder="button link" value="{{$sectionSetting->sec2BtnLink}}">
                                             </div>
                                         </div>
                                         <div class="text-center">
@@ -210,22 +210,22 @@
                                         @csrf
                                         <div class="form-group row">
                                             <div class="col-sm-12">
-                                                <input type="text" id="text" class="form-control" name="sec5Heading" placeholder="heading"  value="{{$sectionSetting->sec5Heading}}">
+                                                <input type="text" id="text" class="form-control" required name="sec5Heading" placeholder="heading"  value="{{$sectionSetting->sec5Heading}}">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <div class="col-sm-12">
-                                                <input type="text" id="text" class="form-control" name="sec5Text" placeholder="sub-heading"  value="{{$sectionSetting->sec5Text}}">
+                                                <input type="text" id="text" class="form-control" required name="sec5Text" placeholder="sub-heading"  value="{{$sectionSetting->sec5Text}}">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <div class="col-sm-12">
-                                                <input type="file" id="file" class="form-control" name="sec5Image"  value="{{$sectionSetting->sec5Image}}">
+                                                <input type="file" id="file" class="form-control" required name="sec5Image"  value="{{$sectionSetting->sec5Image}}">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <div class="col-sm-12">
-                                                <input type="text" id="file" class="form-control" name="sec5BtnLink" placeholder="button link">
+                                                <input type="text" id="file" class="form-control" required name="sec5BtnLink" placeholder="button link">
                                             </div>
                                         </div>
                                         <div class="text-center">
