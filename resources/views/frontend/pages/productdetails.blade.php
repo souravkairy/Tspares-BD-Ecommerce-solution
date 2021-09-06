5cfffc0745d7b3480bbb66e3218c3f23b788686f
naeemchy <61413398+naeemchy@users.noreply.github.com>
2021-09-06 22:58:05 +0600

    header

--- resources/views/frontend/pages/productdetails.blade.php
+++ resources/views/frontend/pages/productdetails.blade.php
@@ -14,20 +14,18 @@
     <div class="col-md-6 col-lg-6 col-xl-6 col-sm-12 p-5" id="content-wrapper">
       <div style="" class="column">
         <div class="product_image mb-3">
-          <img height="400" id=featured class="image" src="" alt="images">
+          <img height="400" id=featured class="image" src="{{ asset(($active_p_image->image)) }}" alt="images">
         </div>
       </div>
       <div class="">
         <div class="product_image_logo" id="slide-wrapper">
           <img id="slideLeft" class="arrow" src="{{ asset('frontend/assets/image/arrow-left.png')}}">
           <div class="d-flex flex-row image_list" id="slider">
-                       @forelse ($p_image as $item)
-          <div class="p-3 logo_image"><img class="image thumbnail active" src="{{ asset(( $item->image)) }}" alt="images"></div>
-
-                       @empty
-
-                       @endforelse
-        </div>
+            <div class="p-3 logo_image"><img class="image thumbnail active" src="{{ asset(($active_p_image->image)) }}" alt="images"></div>
+            @foreach($p_image as $p_img)
+            <div class="p-3 logo_image"><img class="image thumbnail active" src="{{ asset(($p_img->image)) }}" alt="images"></div>
+            @endforeach
+          </div>
           <img id="slideRight" class="arrow" src="{{ asset('frontend/assets/image/arrow-right.png')}}">
         </div>
       </div>
@@ -58,9 +56,14 @@
 
             <p class="color_head">Colour</p>
             <div class="d-flex flex-row">
-              @foreach($p_image as $color)
-                <div class="p-1"><p class="px-3 py-1 btn btn-outline-none color_button text-muted Color">{{ $color->color }}</p></div>
-              @endforeach
+              
+              <div class="d-flex flex-row image_list" id="slider">
+                  <div class="p-1"><p class="px-3 py-1 btn btn-outline-none color_button text-muted Color button_select">{{ $active_p_image->color }}</p></div>
+                  @foreach($p_image as $color)
+                  <div class="p-1"><p class="px-3 py-1 btn btn-outline-none color_button text-muted Color">{{ $color->color }}</p></div>
+                  @endforeach
+              </div>
+               
                 <input type="hidden" name="p_color" value="" id="getColor">
             </div>
             </div>
