8a3e844d7f7c4adc3d0274d95fd3363b4f0c394c
souravkairy <48748661+souravkairy@users.noreply.github.com>
2021-09-07 08:38:31 +0600

    ddf

--- resources/views/frontend/elements/header.blade.php
+++ resources/views/frontend/elements/header.blade.php
@@ -592,16 +592,3 @@ function deleteCartProduct(id) {
 }
 </script>
 
-<script>
-    $('.add').click(function () {
-        if ($(this).prev().val() < 10) {
-        $(this).prev().val(+$(this).prev().val() + 1);
-        }
-    });
-    $('.sub').click(function () {
-            if ($(this).next().val() > 1) {
-            if ($(this).next().val() > 1) $(this).next().val(+$(this).next().val() - 1);
-            }
-    });
-</script>
-
